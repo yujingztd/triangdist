d13edc3d571867ad9f6d2db910fd0577ee6b3b00
yujingztd <yujingz@alumni.uv.es>
2026-04-29 00:48:01 +0200

    ptriang test

--- tests/testthat/test-triang_dist.R
+++ tests/testthat/test-triang_dist.R
@@ -1,3 +1,4 @@
+# TEST dtriang
 test_that("dtriang raises errors when parameters are not valid", {
   # 'min' greater than 'max'
   expect_error(dtriang(x = 1, min = 3, max = 1, mode = 2))
@@ -20,4 +21,21 @@ test_that("dtriang calculates correctly the density", {
 
   # Valid value (ex. x = 2)
   expect_equal(dtriang(2, min = 0, max = 3, mode = 1), 1/3)
+})
+
+
+
+# TESTS ptriang
+test_that("ptriang validates parameters and calculates probabilities", {
+  # Errors
+  expect_error(ptriang(1, min = 3, max = 1, mode = 2))
+  expect_error(ptriang(1, min = 0, max = 3, mode = 4))
+
+  # Operations
+  expect_equal(ptriang(-1, min = 0, max = 3, mode = 1), 0) # Antes del min
+  expect_equal(ptriang(4, min = 0, max = 3, mode = 1), 1)  # Después del max
+  expect_equal(ptriang(1, min = 0, max = 3, mode = 1), 1/3) # Exactamente en la moda
+
+  # Probability (ex. x = 2)
+  expect_equal(ptriang(2, min = 0, max = 3, mode = 1), 5/6)
 })
